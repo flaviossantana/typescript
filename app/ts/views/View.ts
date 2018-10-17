7aa6c009a342a6a65cba627c3e62893d9a14e235
flavio.santana <9090flavio@gmail.com>
2018-10-16 22:31:12 -0300

    jQuery, tem espaço?

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,15 @@
+declare var $: any;
+
 abstract class View<T> {
 
-    private _elemento: Element;
+    protected _elemento: any;
 
     constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T): void {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     abstract template(model: T): string;
